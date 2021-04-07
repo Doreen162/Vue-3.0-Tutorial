0ecb6ebb1109e71b4cdb5d791e32dc17273cbad9
Doreen162 <doreenligman17@gmail.com>
2021-04-07 14:33:17 +0200

    Using props in Setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,24 @@
 <template>
   <div class="home">
    <h1>Home</h1>
-   <input type="text" v-model="search">
-   <p>search term - {{ search }}</p>
-   <div v-for="name in matchingNames" :key="name">{{ name }}</div>
-   <button @click="handleClick">stop watching</button>
+   <PostList :posts="posts" />
   </div>
 </template>
 
 <script>
-import { computed, ref, watch, watchEffect } from 'vue'
+import PostList from '../components/PostList.vue'
+import { ref } from 'vue'
 
 export default {
   name: 'Home',
+  components: { PostList },
   setup() {
-    const search = ref('')
-    const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
-
-    const stopWatch = watch(search, () => {
-      console.log('watch function ran')
-    })
-
-    const stopEffect = watchEffect(() => {
-      console.log('watchEffect function ran', search.value)
-    })
+    const posts = ref([
+      { title: 'welcome to the blog', body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', id: 1},
+      { title: 'top 5 CSS tips', body: 'lorem ipsum', id: 2},
+    ])
    
-    const matchingNames = computed(() => {
-      return names.value.filter((name) => name.includes(search.value))
-    })
-
-    const handleClick = () => {
-      stopWatch()
-      stopEffect()
-    }
-   
-    return { names, search, matchingNames, handleClick }
+    return { posts }
   }
 }
 </script>
